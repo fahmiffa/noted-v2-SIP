c8e0db2cbf67a647dc608d65933aa59060c98e90
fahmi Fa <faisol.ajifa@gmail.com>
2024-09-23 21:43:45 +0700

    document

--- app/Models/Head.php
+++ app/Models/Head.php
@@ -102,7 +102,7 @@ class Head extends Model
         {
             $val = 'Pembuatan Lampiran';
         }
-        else if($this->do == 0 && $barp && $barp->status == 1 && $bak && $bak->status == 1)
+        else if($this->do == 0 && $barp && $this->barp->status == 1 && $bak && $this->bak->status == 1)
         {
             $val = 'Verifikasi Kabid / Ketua TPA';
         }
