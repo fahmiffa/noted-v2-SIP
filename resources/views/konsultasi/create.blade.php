e4e55b565bf5d4306b158c8cb4901710c66473c1
fahmi Fa <faisol.ajifa@gmail.com>
2024-09-23 15:59:15 +0700

    other

--- resources/views/konsultasi/create.blade.php
+++ resources/views/konsultasi/create.blade.php
@@ -205,7 +205,7 @@
                                             <select class="form-control" name="place" required>
                                                 <option value="">Pilih Tempat</option>
                                                 @php
-                                                    $var = ['alamat_bangunan', 'ruang_rapat_DPUPR_Kabupaten_Tegal', 'link_zoom'];
+                                                    $var = ['alamat_bangunan', 'ruang_rapat_DPUPR_Kabupaten_Tegal', 'melalui_Daring/Teleconference'];
                                                 @endphp
                                                 @foreach ($var as $item)
                                                     <option value="{{ $item }}" @selected(isset($schedule) && $place[0] == $item) @selected( old('place') == $item)>
