911c42a4afa5625bfce52caaa019654cdd303cc0
fahmi Fa <faisol.ajifa@gmail.com>
2024-09-24 14:31:38 +0700

    pdf

--- resources/views/document/bak/doc/index.blade.php
+++ resources/views/document/bak/doc/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layout.pdf')
 @section('main')
-<img src=" {{ gambar('bg.png') }} " class="watermark" alt="Watermark" />
+<img src=" {{ gambar('bg.png') }} " class="watermark" alt="Watermark" width="100%" />
     @include('document.bak.doc.home');
 @endsection
