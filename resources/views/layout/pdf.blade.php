fe90c07fa4a33ce83893a96fbd3002276deb7d7e
fahmi Fa <faisol.ajifa@gmail.com>
2024-09-24 14:13:15 +0700

    pdf

--- resources/views/layout/pdf.blade.php
+++ resources/views/layout/pdf.blade.php
@@ -35,6 +35,8 @@
         position: absolute;
         top: 0;
         left: 0;
+        right: 0;
+        bottom: 0;
         width: 100%;
         height: 100%;
         background-image: url({{gambar('watermak.png')}});
