990463981cbad13f84fdd49254003cc078b1c2a6
fahmi Fa <faisol.ajifa@gmail.com>
2024-09-24 14:18:04 +0700

    pdf

--- resources/views/document/bak/doc/index.blade.php
+++ resources/views/document/bak/doc/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layout.pdf')
 @section('main')
-    <div class="watermark"></div>
+<img src=" {{gambar('bg.png')}} " class="watermark" alt="Watermark" />
     @include('document.bak.doc.home');
 @endsection
--- resources/views/layout/pdf.blade.php
+++ resources/views/layout/pdf.blade.php
@@ -33,19 +33,13 @@
 
     .watermark {
         position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        width: 100%;
-        height: 100%;
-        background-image: url({{gambar('bg.png')}});
-        background-repeat: repeat;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
         opacity: 0.1;
-        pointer-events: none;
-        z-index: 9999;
-        transform: rotate(-5deg);
-        transform-origin: 0 0;
+        /* Adjust transparency */
+        z-index: -1;
+        /* Ensure it stays behind other content */
     }
 
     .img {
