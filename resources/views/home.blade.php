4073f1f2c45123925ffe87143f748d8e64df8347
fahmi Fa <faisol.ajifa@gmail.com>
2024-09-24 20:25:42 +0700

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,26 +16,13 @@
                             <div class="col col-md-4">
                                 <div class="d-flex jusitify-content-between">
                                     <div class="p-1">
-                                        <div class="stats-icon green mb-2">
-                                            <i class="iconly-boldPaper"></i>
-                                        </div>
-                                    </div>
-                                    <div class="p-1">
-                                        <h6 class="text-muted font-semibold">Done</h6>
-                                        <h6 class="font-extrabold mb-0">{{$head->where('finish',1)->count()}}</h6>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col col-md-4">
-                                <div class="d-flex jusitify-content-between">
-                                    <div class="p-1">
                                         <div class="stats-icon red mb-2">
                                             <i class="iconly-boldPaper"></i>
                                         </div>
                                     </div>
                                     <div class="p-1">
-                                        <h6 class="text-muted font-semibold">Progress</h6>
-                                        <h6 class="font-extrabold mb-0">{{$head->where('finish',0)->count()}}</h6>
+                                        <h6 class="text-muted font-semibold">Jumlah Permohonan</h6>
+                                        <h6 class="font-extrabold mb-0">{{$head->where('do',0)->count()}}</h6>
                                     </div>
                                 </div>
                             </div>
@@ -47,7 +34,7 @@
                                         </div>
                                     </div>
                                     <div class="p-1">
-                                        <h6 class="text-muted font-semibold">Verifikasi</h6>
+                                        <h6 class="text-muted font-semibold">Tahap Verifikasi</h6>
                                         <h6 class="font-extrabold mb-0">{{$verif}}</h6>
                                     </div>
                                 </div>
@@ -60,7 +47,7 @@
                                         </div>
                                     </div>
                                     <div class="p-1">
-                                        <h6 class="text-muted font-semibold">Konsultasi</h6>
+                                        <h6 class="text-muted font-semibold">Proses Konsultasi</h6>
                                         <h6 class="font-extrabold mb-0">{{$kons}}</h6>
                                     </div>
                                 </div>
@@ -73,7 +60,7 @@
                                         </div>
                                     </div>
                                     <div class="p-1">
-                                        <h6 class="text-muted font-semibold">BAK</h6>
+                                        <h6 class="text-muted font-semibold">Proses BAK</h6>
                                         <h6 class="font-extrabold mb-0">{{$bak}}</h6>
                                     </div>
                                 </div>
@@ -91,6 +78,19 @@
                                     </div>
                                 </div>
                             </div>
+                            {{-- <div class="col col-md-4">
+                                <div class="d-flex jusitify-content-between">
+                                    <div class="p-1">
+                                        <div class="stats-icon blue mb-2">
+                                            <i class="iconly-boldPaper"></i>
+                                        </div>
+                                    </div>
+                                    <div class="p-1">
+                                        <h6 class="text-muted font-semibold">BARP</h6>
+                                        <h6 class="font-extrabold mb-0">{{$barp}}</h6>
+                                    </div>
+                                </div>
+                            </div> --}}
                         </div>             
                     </div>
                 </div>        
@@ -131,7 +131,7 @@
             },
             plotOptions: {},
             series: [{
-                name: "permohonan",
+                name: "Permohonan",
                 data: @json($chart['counts']),
             }, ],
             colors: "#435ebe",
