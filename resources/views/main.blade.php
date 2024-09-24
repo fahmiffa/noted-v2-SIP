9583ef513727028b6261bbbb8f4b0c7565c1a6b1
fahmi Fa <faisol.ajifa@gmail.com>
2024-09-24 14:55:29 +0700

    pdf

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,8 +4,7 @@
         <div class="page-title">
             <div class="row">
                 <div class="col-12 col-md-6 order-md-1 order-last">
-                    <h3>Dokumen</h3>
-                    <p class="text-subtitle text-muted">Ringkasan Task Dokumen</p>
+                    <h3>Ringkasan</h3>
                 </div>
             </div>
         </div>
