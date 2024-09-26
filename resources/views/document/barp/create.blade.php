d9dd15d071f61360f000355ecb716aea0c945175
fahmi Fa <faisol.ajifa@gmail.com>
2024-09-26 15:33:39 +0700

    margin bottom

--- resources/views/document/barp/create.blade.php
+++ resources/views/document/barp/create.blade.php
@@ -257,6 +257,7 @@
                 ['style', ['bold', 'italic', 'underline', 'clear']],
                 ['font', ['strikethrough', 'superscript', 'subscript']],
                 ['para', ['ul', 'ol', 'paragraph']],
+                ['table', ['table']],
             ]
         });
 
