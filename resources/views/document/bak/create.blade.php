3bd4aca7a13aeb94da7ba05bf07144876c53d87d
fahmi Fa <faisol.ajifa@gmail.com>
2024-09-26 15:39:08 +0700

    margin bottom

--- resources/views/document/bak/create.blade.php
+++ resources/views/document/bak/create.blade.php
@@ -442,6 +442,7 @@
                 ['style', ['bold', 'italic', 'underline', 'clear']],
                 ['font', ['strikethrough', 'superscript', 'subscript']],
                 ['para', ['ul', 'ol', 'paragraph']],
+                ['table', ['table']],
             ]
         });
 
