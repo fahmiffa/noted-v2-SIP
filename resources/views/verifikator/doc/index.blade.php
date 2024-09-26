37aac358679fbdc9565d819b62c7d3b55e4ed7a2
fahmi Fa <faisol.ajifa@gmail.com>
2024-09-26 20:37:24 +0700

    margin bottom

--- resources/views/verifikator/doc/index.blade.php
+++ resources/views/verifikator/doc/index.blade.php
@@ -259,21 +259,7 @@
 
 
     @if ($head->deleted_at)
-        <script type="text/php"> 
-            if (isset($pdf)) { 
-                //Shows number center-bottom of A4 page with $x,$y values
-                $x = 35;  //X-axis i.e. vertical position 
-                $y = 820; //Y-axis horizontal position
-                $text = "Dokumen Perbaikain ke {{$num}}";  
-                $font =  $fontMetrics->get_font("helvetica", "bold");
-                $size = 7;
-                $color = array(255,0,0);
-                $word_space = 0.0;  //  default
-                $char_space = 0.0;  //  default
-                $angle = 0.0;   //  default
-                $pdf->page_text($x, $y, $text, $font, $size, $color, $word_space, $char_space, $angle);
-            }
-        </script>
+        <p class="footer" style="color: red">Dokumen Perbaikain ke {{ $num }}</p>
     @endif
 
     @if ($head->grant == 1)
